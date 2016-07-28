525a6cb4b3fdf68f44cb43f2037b7b4086bec4e4
longtaoge <61852263@qq.com>
2016-07-28 10:31:18 +0800

    添加 Glide示例

--- app/src/main/java/org/xiangbalao/glide/GlideUtils.java
+++ app/src/main/java/org/xiangbalao/glide/GlideUtils.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.orhanobut.hawk.Hawk;
 
 /**
@@ -16,7 +19,7 @@ public class GlideUtils {
 
     public static String slipTag = "it";
 
-    public static void loadImage(Context context, String url, ImageView imageView) {
+    public static void loadImage(final Context context, final String url, final ImageView imageView) {
 
 
         if (url != null) {
@@ -26,7 +29,7 @@ public class GlideUtils {
                 if (Hawk.get(getCacheUrl(url)) == null) {//用图片URL固定的部分获取第一次缓存图片时的全URL
 
                     Hawk.put(getCacheUrl(url), url); //如果没有缓存过，将第图片第一次加载时的URL缓存起来
-                   //第一次加载用全路径从网络上加载
+                    //第一次加载用全路径从网络上加载
                     Glide.with(context)
                             .load(url)
                             .into(imageView);
@@ -34,13 +37,30 @@ public class GlideUtils {
                 } else {
                     //第二次以后能过 url 加载本地缓存图片
                     Glide.with(context)
-                            .load(Hawk.get(getCacheUrl(url)))
+                            .load(String.valueOf(Hawk.get(getCacheUrl(url))))
+                            // 设置加载失败监听，假如由于缓存过期或
+                            // 者被清等客观原因导致加载失败，从最新的url 加载，并更新本地缓存
+                            .listener(new RequestListener<String, GlideDrawable>() {
+                                @Override
+                                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                    Hawk.put(getCacheUrl(url), url); //更新缓存
+                                    Glide.with(context)
+                                            .load(url)
+                                            .into(imageView);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                    return false;
+                                }
+                            })
                             .into(imageView);
                 }
 
 
             } else {
-                 //如果非动态URL 则直接加载即可
+                //如果非动态URL 则直接加载即可
                 Glide.with(context)
                         .load(url)
                         .into(imageView);
